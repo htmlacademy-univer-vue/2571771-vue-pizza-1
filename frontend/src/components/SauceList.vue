ee83dad9c2ec373afd04b9ad59fc13e94825f8b4
Iman-Ahmad <iman.ahmadncd@gmail.com>
2024-12-04 05:16:41 +0300

    Добавление переменных в компонент в проекте Vue-Pizza

---
+++ frontend/src/components/SauceList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      <h2>Выберите соус:</h2>
+      <ul>
+        <li v-for="item in sauces" :key="item.id">
+          <p><strong>{{ item.name }}</strong></p>
+          <p>Цена: {{ item.price }} ₽</p>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "SauceList",
+    props: {
+      sauces: {
+        type: Array,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    margin-bottom: 20px;
+  }
+  </style>
+  
