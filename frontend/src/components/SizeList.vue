28a07ae3d4111a3230a043c7ab1b7ef99817a927
Iman-Ahmad <iman.ahmadncd@gmail.com>
2024-12-04 04:34:59 +0300

    Добавление переменных в компонент в проекте Vue-Pizza

---
+++ frontend/src/components/SizeList.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+      <h2>Выберите размер:</h2>
+      <ul>
+        <li v-for="item in sizes" :key="item.id">
+          <img :src="`/src/assets/img/${item.image}`" :alt="item.name" />
+          <p><strong>{{ item.name }}</strong></p>
+          <p>Множитель: {{ item.multiplier }}</p>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "SizeList",
+    props: {
+      sizes: {
+        type: Array,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    margin-bottom: 20px;
+  }
+  img {
+    width: 50px;
+    height: 50px;
+  }
+  </style>
+  
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,3 @@
-
 import { createRouter, createWebHistory } from "vue-router";
 // import { middlewarePipeline, isAuthenticated } from "@/middlewares";
 
