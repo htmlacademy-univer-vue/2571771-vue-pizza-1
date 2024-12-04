86d701fa038f6097145cf8213db7105fc36278c9
Iman-Ahmad <iman.ahmadncd@gmail.com>
2024-12-04 05:58:04 +0300

    Доба > вление переменных в компонент в проекте Vue-Pizza

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";  // Importing HomeView
+import AboutView from "@/views/AboutView.vue"; // Importing AboutView
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [],
 });
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: HomeView,  // Mapping path '/' to HomeView
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: AboutView,  // Mapping path '/about' to AboutView
+  },
+];
 
 export default router;
