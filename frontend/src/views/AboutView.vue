56553e474bed0e2fd7b67818e6b3fe131a6f8587
Iman-Ahmad <iman.ahmadncd@gmail.com>
2024-12-04 05:16:41 +0300

    Добавление переменных в компонент в проекте Vue-Pizza

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> ee83dad (Добавление переменных в компонент в проекте Vue-Pizza)
     <div>
       <h2>About Vue Pizza</h2>
       <p>This is an about page for the Vue Pizza Project.</p>
@@ -11,6 +14,7 @@
     name: "AboutPage",
   };
   </script>
+<<<<<<< HEAD
   
 =======
   <div>
@@ -25,3 +29,6 @@ export default {
 };
 </script>
 >>>>>>> 928f0bb (initial commit)
+=======
+  
+>>>>>>> ee83dad (Добавление переменных в компонент в проекте Vue-Pizza)
