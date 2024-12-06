62db28330cca955c146f6a65476682fee9a752d2
Iman-Ahmad <iman.ahmadncd@gmail.com>
2024-12-06 07:10:18 +0300

    Добавление переменных в компонент в проекте Vue-Pizza

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,42 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";  // Importing HomeView
+import BasketView from '@/views/BasketView.vue';  // Example for other views
+import RestaurantsView from '@/views/RestaurantsView.vue'; // Example for other views
 import AboutView from "@/views/AboutView.vue"; // Importing AboutView
+import AppHeader from "@/layouts/AppHeader.vue";  // Importing HomeView
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'AppHeader',
+    component: AppHeader
+  },
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView
+  },
+  {
+    path: '/basket',
+    name: 'Basket',
+    component: BasketView
+  },
+  {
+    path: '/restaurants',
+    name: 'Restaurants',
+    component: RestaurantsView
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('@/views/AboutView.vue')
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [],
 });
-
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: HomeView,  // Mapping path '/' to HomeView
-  },
-  {
-    path: "/about",
-    name: "About",
-    component: AboutView,  // Mapping path '/about' to AboutView
-  },
-];
 
 export default router;
--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,13 +1,12 @@
 <template>
-    <div>
-      <h2>About Vue Pizza</h2>
-      <p>This is an about page for the Vue Pizza Project.</p>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "AboutPage",
-  };
-  </script>
-  
+  <div>
+    <h2>About Vue Pizza</h2>
+    <p>This is an about page for the Vue Pizza Project.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AboutPage",
+};
+</script>
