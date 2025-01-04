d08698cc1a3f1ae9c39821a4ac1ea99b107a5bdf
Iman-Ahmad <iman.ahmadncd@gmail.com>
2025-01-04 07:46:22 +0300

    initial commit

---
+++ frontend/src/modules/constructor/DoughSelector.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <h2>Выберите тесто:</h2>
+      <DoughList :dough="doughOptions" @select="handleSelect" />
+    </div>
+  </template>
+  
+  <script>
+  import DoughList from "@/components/DoughList.vue";
+  
+  export default {
+    name: "DoughSelector",
+    components: { DoughList },
+    data() {
+      return {
+        doughOptions: [
+          { id: 1, name: "Тонкое", description: "Хрустящее тесто", price: 50, image: "thin.png" },
+          { id: 2, name: "Пышное", description: "Мягкое тесто", price: 70, image: "thick.png" },
+        ],
+      };
+    },
+    methods: {
+      handleSelect(selectedDough) {
+        this.$emit("select", selectedDough);
+      },
+    },
+  };
+  </script>
+  
